d3818211a9b00447c554becdbb4051769fbdaf07
Jonathan Getahun <kgetahun2@gmail.com>
2020-08-21 19:11:41 -0400

    more addition of boiler plate and authorization

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { auth } = require("./middleware/auth");
 
 const config = require('./config/key')
 
 const { User } = require('./models/user');
+const user = require('./models/user');
 
 mongoose.connect(config.mongoURI, 
     {useNewUrlParser: true}).then(()=>console.log('connected success'))
@@ -16,7 +18,21 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.post('/api/users/register', (req,res) => {
+
+app.get("/api/user/auth", auth, (req,res) => {
+    res.status(200).json({
+        _id: req._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
+})
+
+//request is coming from client (postman when developing) and put
+//data into mongoDB
+app.post('/api/users/register',  (req,res) => {
     const user = new User(req.body)
     user.save()
     .then(() => res.status(200).json(`User '${req.body.name}' added!`))
@@ -24,6 +40,38 @@ app.post('/api/users/register', (req,res) => {
     
 })
 
+app.post('/api/user/login', (req,res) => {
+    //find the email
+    User.findOne({ email: req.body.email}, (err,user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            })
+        }
+
+    })
+    //this.password is the password in teh database
+
+    //compare Password 
+    user.comparePassword(req.body.password, (err, isMatch) => {
+        if(isMatch){
+            return res.json({
+                loginSuccess:false,
+                message:"Auth failed, wrong password"
+            })
+        }
+    })
+    //generate Token
+    user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err)
+        res.cookie("x_auth", user.token)
+            .status(200)
+            .json({
+                loginSuccess: true
+            })
+    })
+})
 app.get('/',(req,res)=> {
     res.send("hello world")
 })
--- models/user.js
+++ models/user.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = mongoose.Schema({
     name:{
@@ -29,6 +33,55 @@ const userSchema = mongoose.Schema({
         type:Number
     }
 })
+
+//before saving you want to hash
+userSchema.pre('save', function( next ) {
+    var user = this;
+    //process happens only when modifying password
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            //next means you go instantly to save aka user.save() in index.js
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            })
+        }) 
+    } else {
+        next();
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    bcrypt.compare(plainPassword,this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(),'secret')
+
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user);
+    })
+}
+
+userSchema.static.findByToken = function(token, cb) {
+    var user = this;
+
+    jwt.verify(token, 'secret', function(err, decode){
+        user.findOne({"_id":decode, "token":token}, function(err, user){
+            if(err) return cb(err);
+            cb(null, user);
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 
